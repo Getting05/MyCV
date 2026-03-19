2ef4dd53f145118eb28924eefb88d5aed4266d86
Getting05 <155835254+Getting05@users.noreply.github.com>
2026-03-19 10:29:28 +0000

    Deploying to gh-pages from @ Getting05/MyCV@756ccc07f45a12a7457b679fc8f12cd2a7549677 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://Getting05.github.io/MyCV/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://cv.getting132.top//#contact">contact us</a>.'
               }
             ]
           }
